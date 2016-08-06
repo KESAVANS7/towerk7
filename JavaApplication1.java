83e67456387346e29f5194254de1907cc2143db8
KESAVANS7 <kesavansubburaj7@gmail.com>
2016-08-06 15:29:21 +0530

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Student
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+	
+	       if (f== 1) 
+	       {
+	           System.out.println(begin + "<==>" + complete);
+	       } 
+	       else 
+	       {
+	           solve(f-1, begin,complete,runs);
+	           System.out.println(begin+ " <==>" +complete);
+	           solve(f-1,runs,begin,complete);
+	       }
+	   }
+	 
+    }
+
